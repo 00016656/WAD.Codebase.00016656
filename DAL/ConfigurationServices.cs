d2dd28cd8753f7f88e108f2f42ef311cfb2653d2
00016656 <wiut00016656@gmail.com>
2024-11-28 22:38:44 +0500

    Data access layer implementation

---
+++ DAL/ConfigurationServices.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using WAD.Codebase._00016656.Data;
+using WAD.Codebase._00016656.Services.Interfaces;
+using WAD.Codebase._00016656.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public static class ConfigurationServices
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add services to the container.
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IPropertyRepository, PropertyRepository>();
+            services.AddDbContext<RealEstateDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            return services;
+        }
+    }
+}
--- WAD.Codebase.00016656/Program.cs
+++ WAD.Codebase.00016656/Program.cs
@@ -1,18 +1,9 @@
-using WAD.Codebase._00016656.Services.Interfaces;
-using WAD.Codebase._00016656.Services;
-using Microsoft.Extensions.Configuration;
-using WAD.Codebase._00016656.Data;
-using Microsoft.EntityFrameworkCore;
+using DAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IPropertyRepository, PropertyRepository>();
-builder.Services.AddDbContext<RealEstateDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.ConfigureServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
